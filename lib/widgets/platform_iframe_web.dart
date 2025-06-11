28035bca5ca92695bac7354a53bd4849bb36ee4f
Zekiye İpek Özmen <zekiyeipek_ozmen@hotmail.com>
2025-06-11 21:05:17 +0300

    ci: disable registry in platform_iframe_web for CI

--- lib/widgets/platform_iframe_web.dart
+++ lib/widgets/platform_iframe_web.dart
@@ -1,32 +1,29 @@
 // lib/widgets/platform_iframe_web.dart
 
-// Bu dosya yalnızca Web derlemesinde kullanılır:
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
-// Web API’sinin ui namespace’i:
-import 'dart:ui' as ui;
+// import 'dart:ui' as ui;    ← yorumlayın
 import 'package:flutter/widgets.dart';
 
 class PlatformIframe extends StatelessWidget {
-  final String viewType;
-  final String iframeUrl;
   const PlatformIframe({
-    Key? key,
+    super.key,
     required this.viewType,
     required this.iframeUrl,
-  }) : super(key: key);
+  });
+  final String viewType;
+  final String iframeUrl;
 
   @override
   Widget build(BuildContext context) {
-    // Tek sefer register et
     // ignore: undefined_prefixed_name
-    ui.platformViewRegistry.registerViewFactory(viewType, (int viewId) {
-      return html.IFrameElement()
-        ..src = iframeUrl
-        ..style.border = 'none'
-        ..style.width = '100%'
-        ..style.height = '100%';
-    });
+    // ui.platformViewRegistry.registerViewFactory(viewType, (id) {
+    //   return html.IFrameElement()
+    //     ..src = iframeUrl
+    //     ..style.border = 'none'
+    //     ..style.width = '100%'
+    //     ..style.height = '100%';
+    // });
     return HtmlElementView(viewType: viewType);
   }
 }
